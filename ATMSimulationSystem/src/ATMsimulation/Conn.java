19fb3eee2bd5b6df763cc8b3890c2a982d9b205d
Bhavik Patwa <112922239+Bhavik-Patwa@users.noreply.github.com>
2022-11-29 19:12:25 +0530

    Update Conn.java

--- ATMSimulationSystem/src/ATMsimulation/Conn.java
+++ ATMSimulationSystem/src/ATMsimulation/Conn.java
@@ -8,7 +8,7 @@ public class Conn{
     public Conn(){
         try{
             Class.forName("com.mysql.jdbc.Driver");
-            c =DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem","root","password");
+            c =DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem","root","password"); // MySQL WorkBench connection details.
             s =c.createStatement();
         }catch(Exception e){
             System.out.println(e);
